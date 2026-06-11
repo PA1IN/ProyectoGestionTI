4f40d54fb6298166c83662492b8c803e9ec9d3ed
Isaias Gavia <isaiat.gg@gmail.com>
2026-06-11 00:25:59 -0400

    Fix: Conciliacion query error returning 0

--- Backend/apps/conciliacion/src/main.ts
+++ Backend/apps/conciliacion/src/main.ts
@@ -4,6 +4,7 @@ import { ConciliacionModule } from './conciliacion.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ConciliacionModule);
+  app.enableCors();
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT_CONCILIACION') || 3002;
   await app.listen(port);
