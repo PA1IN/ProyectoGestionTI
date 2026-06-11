6e22ffe6859b1c71da390a279d5d88564e1fa9b7
Isaias Gavia <isaiat.gg@gmail.com>
2026-06-11 02:01:07 -0400

    Revert "Fix: Conciliacion query error returning 0"
    This reverts commit 4f40d54fb6298166c83662492b8c803e9ec9d3ed.

--- Backend/apps/conciliacion/src/main.ts
+++ Backend/apps/conciliacion/src/main.ts
@@ -4,7 +4,6 @@ import { ConciliacionModule } from './conciliacion.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ConciliacionModule);
-  app.enableCors();
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT_CONCILIACION') || 3002;
   await app.listen(port);
